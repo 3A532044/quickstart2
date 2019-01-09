e8916c8a5456cbc64020ccc8044d4f1e8c2611ef
林宥丞 <a26913700@gmail.com>
2019-01-09 21:09:35 +0800

    顯示已有的任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Task;
 class TaskController extends Controller
 {
     public function __construct()
@@ -12,7 +12,11 @@ class TaskController extends Controller
     }
     public function index(Request $request)
     {
-        return view('tasks.index');
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
     }
     public function store(Request $request)
     {
