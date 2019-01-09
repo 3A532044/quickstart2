3af7c47f3e90e33604e23e85676be5ffb5cfa2bc
林宥丞 <a26913700@gmail.com>
2019-01-09 21:01:50 +0800

    控制器加入store方法

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,4 +14,10 @@ class TaskController extends Controller
     {
         return view('tasks.index');
     }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+    }
 }
